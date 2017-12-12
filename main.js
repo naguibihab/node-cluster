5a5a2b042a9dae276ea30a647b6cb27377840644
Nick Ihab <n.ihab@learnmeter.com>
2017-12-12 15:11:25 +1100

    Init. Exploring the cluster concept https://nodejs.org/api/cluster.html

---
+++ main.js
@@ -0,0 +1,27 @@
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  // console.log('cluster of workers',cluster.workers);
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+  http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('hello world\n'+process.pid);
+  }).listen(process.pid);
+
+  console.log(`Worker ${process.pid} started`);
+}
